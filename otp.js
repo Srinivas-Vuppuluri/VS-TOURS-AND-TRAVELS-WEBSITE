accd0f87e04c042a814cadc25e924fbd09f5dbb9
Srinivas-Vuppuluri <159790218+Srinivas-Vuppuluri@users.noreply.github.com>
2024-03-07 18:08:54 +0530

    Create otp.js

---
+++ otp.js
@@ -0,0 +1,14 @@
+document.querySelector("#form").addEventListener("submit",formsubmit);
+    var arr=JSON.parse(localStorage.getItem("carddetails")) || [];
+    
+    function formsubmit(event){
+        event.preventDefault();
+        var pass=document.querySelector("#pass").value;
+        if(pass="1234"){
+            alert("your payment succuss");
+        }
+        else{
+            alert(" incorrect OTP");
+        }
+        window.location.href="index.html";
+    }
