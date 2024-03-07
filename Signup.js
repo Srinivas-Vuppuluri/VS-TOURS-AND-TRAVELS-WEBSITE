c48c1041b6b8882aa79c64d06221df574e0612f4
Srinivas-Vuppuluri <159790218+Srinivas-Vuppuluri@users.noreply.github.com>
2024-03-07 16:49:46 +0530

    Update Signup.js

--- Signup.js
+++ Signup.js
@@ -1,1 +1,24 @@
-
+document.querySelector("#form").addEventListener("submit",formsubmit);
+var arr=JSON.parse(localStorage.getItem("data")) || [];
+function formsubmit(event){
+    event.preventDefault();
+    var name=document.querySelector("#user").value;
+    var email=document.querySelector("#email").value;
+    var mobile=document.querySelector("#mobile").value;
+    var pass=document.querySelector("#pass").value;
+    
+var obj={userName:name, emailAdd:email, mobileNum:mobile, passWord:pass,};
+arr.push(obj)
+localStorage.setItem("data",JSON.stringify(arr));
+for( var i=0;i<arr.length;i++){
+    if(arr[i].userName==name && arr[i].emailAdd==email && arr[i].mobileNum==mobile && arr[i].passWord==pass){
+        alert("sign up sucess");
+      window.location.href="login.html";
+    }
+//  if(arr[i].userName=="" || arr[i].emailAdd=="" || arr[i].mobileNum=="" || arr[i].passWord==""){
+//   alert("please fill the details");
+  
+//   window.location.href="Signup.html";
+// }
+}
+}
